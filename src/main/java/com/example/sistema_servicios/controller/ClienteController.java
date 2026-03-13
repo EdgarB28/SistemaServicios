d838b6195837ec39423c16299b10a2d9c2094da3
Edgar Galvez <edgarbont5k@gmail.com>
2026-03-13 00:40:13 -0500

    feat: segurity

--- src/main/java/com/example/sistema_servicios/controller/ClienteController.java
+++ src/main/java/com/example/sistema_servicios/controller/ClienteController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ClienteController {
     private final ClienteService clienteService;
 
